74c89d5252b0d06c6a6fc352e86fe3d7145cc643
Gizar <gizarir@gmail.com>
2022-10-19 16:43:05 +0300

    App-bboard: setuped TINYMCE

--- project/settings.py
+++ project/settings.py
@@ -144,7 +144,32 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
 TINYMCE_DEFAULT_CONFIG = {
-    "height": "200px",
+    "height": "250px",
     "width": "750px",
+    'cleanup_on_startup': True,
+    'custom_undo_redo_levels': 20,
+    'selector': 'textarea',
+    'browser_spellcheck': 'true',
+    # 'theme': 'modern',
+    'plugins': '''
+        textcolor save link image media preview codesample contextmenu
+        table code lists fullscreen  insertdatetime  nonbreaking
+        contextmenu directionality searchreplace wordcount visualblocks
+        visualchars code fullscreen autolink lists  charmap print  hr
+        anchor pagebreak
+        ''',
+    'toolbar1': '''
+        fullscreen preview bold italic underline | fontselect,
+        fontsizeselect  | forecolor backcolor | alignleft alignright |
+        aligncenter alignjustify | indent outdent | bullist numlist table |
+        | link image media | codesample
+        ''',
+    # 'toolbar2': '''
+    #     visualblocks visualchars |
+    #     charmap hr pagebreak nonbreaking anchor |  code |
+    #     ''',
+    'contextmenu': 'formats | link image',
+    'menubar': True,
+    'statusbar': True,
 }
 # End AFP
