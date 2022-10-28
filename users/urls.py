0089d565a92c91a9529439325c71bb8b315fdbbe
Gizar <gizarir@gmail.com>
2022-10-28 12:39:06 +0300

    App-users: added user_context to LoginView

--- users/urls.py
+++ users/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path, include
 from django.contrib.auth.urls import *
 
-from users.views import Register
+from users.views import Register, LoginView
 
 urlpatterns = [
-    path('', include('django.contrib.auth.urls')),
+    path('login/', LoginView.as_view(), name="login"),
     path('register/', Register.as_view(), name='register'),
+    path('', include('django.contrib.auth.urls')),
+
 ]
--- users/views.py
+++ users/views.py
@@ -1,18 +1,41 @@
 from django.contrib.auth import authenticate, login
+from django.contrib.auth.views import LoginView
 # from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import render, redirect
 from django.views import View
 
 from users.forms import UserCreationForm
 
+from bboard.utils import DataMixin
 
-class Register(View):
+class LoginView(DataMixin,LoginView):
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        c_def = self.get_user_context(title="Вход на портал")
+        context = dict(list(context.items()) + list(c_def.items()))
+        print(context)
+        return context
+
+
+class Register(DataMixin, View):
 
     template_name = 'registration/register.html'
 
     def get(self, request):
+        # context = {
+        #     'form': UserCreationForm
+        # }
+        # form = self.form_class()
+        categories = self.get_user_context()['categories']
+        menu = self.get_user_context()['menu']
+        cat_selected = self.get_user_context()['cat_selected']
+        # print(post_slug)
         context = {
-            'form': UserCreationForm
+            'form': UserCreationForm,
+            'menu': menu,
+            'categories': categories,
+            'cat_selected':cat_selected,
+            # 'post_slug': post_slug
         }
         return render(request, self.template_name, context)
 
